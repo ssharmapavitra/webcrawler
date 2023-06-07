72232266ad066a162cf854de891ae946a12a955b
ssharmapavitra <80170208+ssharmapavitra@users.noreply.github.com>
2023-06-07 17:56:29 +0530

    Handled Exceptions

--- app.js
+++ app.js
@@ -1,98 +1,100 @@
-//Importing Modules
 const http = require("http");
 const https = require("https");
 const fs = require("fs");
 const readline = require("readline-sync");
 
-//For accepting response from command line
-var url = readline.question("Enter the URL :- ");
-// console.log("Your URL is " + url + "And type is " + typeof url);
+try {
+	// For accepting response from the command line
+	var url = readline.question("Enter the URL: ");
 
-//Variables
-var urls = [];
-var id = 1;
-var curr_count = 1,
-	idx = 0,
-	curr_crawl = 1,
-	temp = 0;
+	// Variables
+	var urls = [];
+	var id = 0;
+	var curr_count = 1,
+		idx = -1,
+		curr_crawl = 1,
+		temp = 0;
 
-urls.push(url);
-main();
+	urls.push(url);
+	main();
 
-async function main() {
-	while (idx < urls.length && curr_crawl < 5) {
-		url = urls[idx];
-		//Choosing http or https
-		let httpClient = url.startsWith("https") ? https : http;
-		console.log("This is  " + url);
+	async function main() {
+		while (idx < urls.length && curr_crawl < 3) {
+			id++;
+			idx++;
+			url = urls[idx];
+			// Choosing http or https
+			let httpClient = url.startsWith("https") ? https : http;
+			console.log("This is " + url);
 
-		await new Promise((resolve, reject) => {
-			// Saving the WebPage of entered URL
-			httpClient
-				.get(url, (res) => {
-					let html = "";
-					res.on("data", (d) => {
-						html += d;
-					});
+			try {
+				await new Promise((resolve, reject) => {
+					// Saving the WebPage of the entered URL
+					httpClient
+						.get(url, (res) => {
+							let html = "";
+							res.on("data", (d) => {
+								html += d;
+							});
 
-					res.on("end", () => {
-						fs.writeFile(`./crawledFiles/${id}.html`, html, (err) => {
-							if (err) {
-								console.error("Error saving file: ", err);
-								reject(err);
-							} else {
-								console.log("Page saved successfully");
-								resolve();
-							}
+							res.on("end", () => {
+								fs.writeFile(`./crawledFiles/${id}.html`, html, (err) => {
+									if (err) {
+										console.error("Error saving file: ", err);
+										reject(err);
+									} else {
+										console.log("Page saved successfully");
+										resolve();
+									}
+								});
+							});
+						})
+						.on("error", (e) => {
+							console.error("Error: ", e);
+							reject(e);
 						});
-					});
-				})
-				.on("error", (e) => {
-					console.error("Error: ", e);
-					reject(e);
 				});
-		});
 
-		//Finding URLs from the given file
-		await new Promise((resolve, reject) => {
-			// Finding URLs from the given file
-			fs.readFile(`./crawledFiles/${id}.html`, "utf8", (err, data) => {
-				if (err) {
-					console.error("Error: ", err);
-					reject(err);
-					return;
-				}
+				await new Promise((resolve, reject) => {
+					// Finding URLs from the given file
+					fs.readFile(`./crawledFiles/${id}.html`, "utf8", (err, data) => {
+						if (err) {
+							console.error("Error: ", err);
+							reject(err);
+							return;
+						}
 
-				// Pattern to match URLs
-				const urlPattern = /https?:\/\/[^\s/$.?#].[^\s]*/gi;
+						// Pattern to match URLs
+						const urlPattern = /https?:\/\/[^\s/$.?#].[^\s]*/gi;
 
-				// Find URLs
-				let list = data.match(urlPattern);
+						// Find URLs
+						let list = data.match(urlPattern);
 
-				// Print URLs
-				// console.log("URLs found in file: ", list);
+						// Update variables
+						if (list != null) {
+							temp += list.length;
+							urls = urls.concat(list);
+							console.log(
+								"This is idx " + idx + "  " + urls.length + "  " + list.length
+							);
+						}
 
-				// Update variables
-				id++;
-				idx++;
-				if (list != null) {
-					temp += list.length;
-					urls = urls.concat(list);
-					console.log(
-						"This is idx " + idx + "  " + urls.length + "  " + list.length
-					);
-				}
-
-				if (id === curr_crawl) {
-					curr_crawl++;
-					id = 1;
-					curr_crawl = temp;
-					temp = 0;
-				}
-				resolve();
-			});
-		});
+						if (id === curr_count) {
+							curr_crawl++;
+							id = 1;
+							curr_count = temp;
+							temp = 0;
+						}
+						resolve();
+					});
+				});
+			} catch (error) {
+				console.error("An error occurred: ", error);
+			}
+		}
 	}
+} catch (error) {
+	console.error("An error occurred: ", error);
 }
 
 // *
