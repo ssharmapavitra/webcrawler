271996b380d08fd85c691880142dbd5cf99f7eff
ssharmapavitra <80170208+ssharmapavitra@users.noreply.github.com>
2023-07-24 17:48:01 +0530

    Added Absolute URL

--- hyperLinkExtractor/HyperlinkExtractor.cpp
+++ hyperLinkExtractor/HyperlinkExtractor.cpp
@@ -4,10 +4,16 @@
 HyperlinkExtractor::HyperlinkExtractor(const std::string &content, const std::string &baseUrl)
     : content_(content), baseUrl_(baseUrl)
 {
+    // remove / from the end of the url
+    if (baseUrl_.back() == '/')
+    {
+        baseUrl_.pop_back();
+    }
 }
 
 std::vector<std::string> HyperlinkExtractor::extractHyperlinks()
 {
+    // std::cout << "Check" << std::endl;
     std::vector<std::string> hyperlinks;
     std::regex linkRegex("<a\\s+(?:[^>]*?\\s+)?href=\"([^\"]*)\"");
     std::smatch match;
@@ -18,8 +24,16 @@ std::vector<std::string> HyperlinkExtractor::extractHyperlinks()
     while (std::regex_search(it, end, match, linkRegex))
     {
         std::string url = match[1];
-        hyperlinks.push_back(makeAbsoluteUrl(url));
-        it = match.suffix().first;
+        std::string absoluteUrl = makeAbsoluteUrl(url);
+        if (!absoluteUrl.empty())
+        {
+            hyperlinks.push_back(absoluteUrl);
+            it = match.suffix().first;
+        }
+        else
+        {
+            it = match[0].second;
+        }
     }
 
     return hyperlinks;
@@ -27,11 +41,14 @@ std::vector<std::string> HyperlinkExtractor::extractHyperlinks()
 
 std::string HyperlinkExtractor::makeAbsoluteUrl(const std::string &url)
 {
-    // ignore #Urls
-    if (url.find("#") != std::string::npos)
+
+    // std::cout << "url: " << url << std::endl;
+    // ignore #Urls and url of size 1 and 0
+    if (url.find("#") != std::string::npos || url.size() <= 1)
     {
         return "";
     }
+
     // Check if the URL is relative
     if (url.empty() || url.find("://") != std::string::npos)
     {
@@ -39,18 +56,5 @@ std::string HyperlinkExtractor::makeAbsoluteUrl(const std::string &url)
     }
 
     // Resolve relative URL using base URL
-    if (baseUrl_.empty() || baseUrl_.back() == '/')
-    {
-        return baseUrl_ + url;
-    }
-    else
-    {
-        size_t lastSlashPos = baseUrl_.rfind('/');
-        if (lastSlashPos != std::string::npos)
-        {
-            return baseUrl_.substr(0, lastSlashPos + 1) + url;
-        }
-    }
-
-    return url; // If resolving fails, return the original URL
+    return baseUrl_ + url;
 }
